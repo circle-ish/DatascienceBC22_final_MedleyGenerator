b96ac8b962bb074de482b5226ce6388f0835cf7f
Charlie <circle-ish@protonmail.com>
2022-08-01 15:07:21 +0200

    suppressed deprecation warninngs

--- src/ProxyServer.py
+++ src/ProxyServer.py
@@ -1,17 +1,39 @@
 # https://proxybroker.readthedocs.io
 
 class ProxyServer():
+    from src.utils import PrintLogger
     
-    def __init__(self, async_handler, proxy_file_path, no_of_proxies = 5):
+    def __init__(
+                    self,
+                    _async_handler, 
+                    proxy_file_path = None, 
+                    no_of_proxies = 5, 
+                    dump_info = PrintLogger.register('ProxyServer')):
+        
         if not proxy_file_path:
             proxy_file_path = 'PROXY_LIST.txt'
             
         self.path = proxy_file_path
         self.no_of_proxies = 5
-        self.ash = async_handler
+        self.ash = _async_handler
+        self.dump_info = dump_info
         
         self.proxy_queue = self.ash.add_queue('proxies')
-
+        
+        from src.utils import install_pip_pkg
+        install_pip_pkg({'proxybroker'})
+        import warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=DeprecationWarning)
+            from proxybroker import Broker
+        
+        self.broker = Broker(self.proxy_queue)
+            
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.dump_info().log('Exiting ProxyServer')     
+        if exc_type:
+            print(exc_type, exc_value, exc_traceback)
+            
     async def get_random_proxy(self):
         """
         Get random proxy from proxy queue
@@ -32,50 +54,18 @@ class ProxyServer():
                 yield line
 
     async def find_proxies(self):
-        
-        # needs to be here before importing the broker; see open issue (Jul '22):
-        # https://github.com/streamlit/streamlit/issues/744#issuecomment-686712930
-        #loop = asyncio.new_event_loop()
-        #asyncio.set_event_loop(loop)
-        
-        from src.utils import install_pip_pkg
-        install_pip_pkg({'proxybroker'})
-        
-        from proxybroker import Broker
-
         european_country_codes = ['DE', 'AT', 'FR', 'UK', 'IT', 'HU', 'IE', 'GR', 'LV', 'LT', 'NL', 'PL', 'RO', 'SK', 'SI', 'ES', 'SE', 'BE', 'BG', 'HR', 'DK', 'EE', 'FI']
-        broker = Broker(self.proxy_queue)
-        producer = self.ash.create_task(broker.find(
+        producer = self.ash.create_task(self.broker.find(
                                                     types=['HTTPS'], 
                                                     limit= self.no_of_proxies, 
                                                     countries = european_country_codes))
-        #consumer = self.ash.create_task(self.get_random_proxy(proxies))
         
-        #await self.ash.gather(producer)
-        #await proxies.join()
-        #consumer.cancel()
-        
-        #tasks = asyncio.gather(broker.find(
-        #                            types=['HTTPS'], 
-        #                            limit= self.no_of_proxies, 
-        #                            countries = european_country_codes),
-        #                       self.save(proxies, filename= self.path))
-        #self.loop = asyncio.get_event_loop()
-        #try:
-        #    asyncio.run(tasks)
-        #    self.loop.run_until_complete(tasks)
-        #except RuntimeError as rte:
-        #    print(rte)
-        #finally:
-        #    self.loop.stop()
-        #    pass
-
-        # following this example
-        # https://github.com/LonamiWebs/Telethon/issues/825#issuecomment-395008836
-        def __exit__(self, exc_type, exc_value, exc_traceback):
-            '''if exc_type and self.loop:
-                pending = asyncio.all_tasks(self.loop)
-                for task in pending:
-                    task.cancel()
-                self.loop.run_until_complete(asyncio.gather(*pending, loop=self.loop))
-                self.loop.close()'''
+    # following this example
+    # https://github.com/LonamiWebs/Telethon/issues/825#issuecomment-395008836
+    #def __exit__(self, exc_type, exc_value, exc_traceback):
+        '''if exc_type and self.loop:
+            pending = asyncio.all_tasks(self.loop)
+            for task in pending:
+                task.cancel()
+            self.loop.run_until_complete(asyncio.gather(*pending, loop=self.loop))
+            self.loop.close()'''
